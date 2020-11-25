4b2b646ee3c9ee5d9b68643c2ed00f77ea1ebd65
thanh-nguyen.dang <thanh-nguyen.dang@ekino.com>
2020-11-25 17:21:21 +0700

    feat(color picker): Implemented color picker for setting page.

---
+++ sample/src/components/colorpicker/java/com/jaredrummler/android/colorpicker/interfaces/ActivityRequestHandler.java
@@ -0,0 +1,20 @@
+package com.jaredrummler.android.colorpicker.interfaces;
+
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+
+public interface ActivityRequestHandler {
+
+    void handlePermissionsRequest(ActivityResultHandler resultHandler, String... permissions);
+
+    void handleActivityRequest(ActivityResultHandler resultHandler, Intent intent);
+
+    @Nullable
+    FragmentManager requestFragmentManager();
+
+    @Nullable
+    PickerTheme getPickerTheme();
+
+}
---
+++ sample/src/components/colorpicker/java/com/jaredrummler/android/colorpicker/utils/SeekBarDrawable.java
@@ -0,0 +1,28 @@
+package com.jaredrummler.android.colorpicker.utils;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.Drawable;
+
+class SeekBarDrawable extends ClipDrawable {
+    private float height = (float)DimenUtils.dpToPx(2.0F);
+    private Rect rect;
+
+    public SeekBarDrawable(Drawable drawable) {
+        super(drawable, 8388611, 1);
+    }
+
+    public void draw(Canvas canvas) {
+        if (this.rect == null) {
+            Rect bounds = this.getBounds();
+            this.setBounds(this.rect = new Rect(bounds.left, (int)((float)bounds.centerY() - this.height / 2.0F), bounds.right, (int)((float)bounds.centerY() + this.height / 2.0F)));
+        }
+
+        super.draw(canvas);
+    }
+
+    public int getOpacity() {
+        return -3;
+    }
+}
