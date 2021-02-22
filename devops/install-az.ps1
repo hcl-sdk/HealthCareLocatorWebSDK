79b2dd27eb6fd1d8d230e8c1c1f12774c5b25e71
Emmanuel Valentin <EValentin@fr.imshealth.com>
2021-02-22 09:00:36 +0000

    Added install-az.ps1

---
+++ devops/install-az.ps1
@@ -0,0 +1,17 @@
+$global:ProgressPreference = 'SilentlyContinue'
+$global:VerbosePreference = "SilentlyContinue"
+
+$azureRmModule = Get-InstalledModule AzureRM -ErrorAction SilentlyContinue
+if ($azureRmModule) {
+  Write-Host 'AzureRM module exists. Removing it'
+  Uninstall-Module -Name AzureRM -AllVersions
+  Write-Host 'AzureRM module removed'
+}
+
+Write-Host 'Installing Az module...'
+Install-Module Az -Force -AllowClobber
+
+if (Get-Command Uninstall-AzureRm -ErrorAction SilentlyContinue) {
+  Write-Host 'Running Uninstall-AzureRm...'
+  Uninstall-AzureRm
+}
