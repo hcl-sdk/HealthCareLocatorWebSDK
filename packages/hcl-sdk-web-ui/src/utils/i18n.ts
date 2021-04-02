bb83baa85d39e62dbf1a71d01408363968cf659e
Tran Cong Luc <cong-luc.tran@ekino.com>
2021-04-02 14:22:45 +0700

    chore: delete i18n json data

--- packages/hcl-sdk-web-ui/src/utils/i18n.ts
+++ packages/hcl-sdk-web-ui/src/utils/i18n.ts
@@ -1,9 +1,21 @@
 import { i18nStore, configStore } from '../core/stores'
 
+const mapFilenameByLang = {
+  'en': 'en_us',
+  'fr': 'fr_fr'
+}
+
+function getLangJsonFileName(lang: string) {
+  const langSanitize = lang.toLowerCase().replace(new RegExp('-', 'ig'), '_');
+  const fileName = mapFilenameByLang[langSanitize] || langSanitize;
+  return `${fileName}.json`;
+}
+
 export async function getI18nLabels(lang: string) {
   try {
     const path = configStore.state.i18nBundlesPath;
-    const resp = await fetch(`${path}/${lang}.json`);
+    const jsonFileName = getLangJsonFileName(lang);
+    const resp = await fetch(`${path}/${jsonFileName}`);
     const i18nLabels = await resp.json();
     i18nStore.setState({ labels: i18nLabels, lang });
   } catch (err) {
