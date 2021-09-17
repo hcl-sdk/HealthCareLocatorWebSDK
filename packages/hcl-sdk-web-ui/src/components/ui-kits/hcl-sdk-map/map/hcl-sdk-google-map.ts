2c94d3209c0840e00b0e4ec87a8fbf7cedca9e45
Tran Cong Luc <cong-luc.tran@ekino.com>
2021-09-17 15:44:23 +0700

    fix(sdk-297): modify the position of zoom control for google map

--- packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-map/map/hcl-sdk-google-map.ts
+++ packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-map/map/hcl-sdk-google-map.ts
@@ -31,6 +31,10 @@ export class HclSdkMapGoogleMap implements IHclSdkMap {
 
     this.map = new google.maps.Map(mapElm, {
       ...options,
+      fullscreenControl: false,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT,
+      },
       styles: [
         {
           featureType: 'landscape.man_made',
