96fa869c7814d3ef4774b1e498923a52da4948e7
tuan-minh.nguyen <tuan-minh.nguyen@ekino.com>
2021-09-29 11:25:18 +0700

    feat(angular-example): integrate devtool in angular example

--- examples/web/angular/src/app/app.module.ts
+++ examples/web/angular/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HclSdkModule } from 'hcl-sdk';
-
+import { HclSdkModule } from '@healthcarelocator/sdk-angular';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { DevToolComponent } from './dev-tool/dev-tool.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HclSdkModule
-  ],
+  declarations: [AppComponent, HomeComponent, SearchComponent, DevToolComponent],
+  imports: [BrowserModule, HclSdkModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
