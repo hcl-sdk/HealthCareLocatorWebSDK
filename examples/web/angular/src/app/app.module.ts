5ad5a1022fb3bfe39923570413d264e37689dd29
tuan-minh.nguyen <tuan-minh.nguyen@ekino.com>
2021-10-12 14:01:32 +0700

    fix(angular-sample): add base href for angular sample app

--- examples/web/angular/src/app/app.module.ts
+++ examples/web/angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HclSdkModule } from '@healthcarelocator/sdk-angular';
+import { APP_BASE_HREF } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,7 @@ import { DevToolComponent } from './dev-tool/dev-tool.component';
 @NgModule({
   declarations: [AppComponent, HomeComponent, SearchComponent, DevToolComponent],
   imports: [BrowserModule, HclSdkModule, AppRoutingModule],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/sample/angular'}],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
--- examples/web/angular/src/app/home/home.component.ts
+++ examples/web/angular/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `<div class="home-content">
-    <img src="/assets/logo-health-care-locator.png" alt="logo" />
+    <img src="/sample/angular/assets/logo-health-care-locator.png" alt="logo" />
     <section class="home-text">
       <p>This sample app is a demo of the integration of HealthCare Locator SDK UI inside a simple server side rendered web application, using Angular.</p>
     </section>
