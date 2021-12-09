38088d335493c7b8c09544c4c2bdc3067ed1e989
Tran Cong Luc <cong-luc.tran@ekino.com>
2021-12-09 17:11:51 +0700

    feat(sdk-589): add 9 new countries and flags

---
+++ packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-icon-flag/AT.tsx
@@ -0,0 +1,11 @@
+import { h } from '@stencil/core';
+
+export default () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 513 342" width="1em" height="1em">
+      <rect y="114" fill="#FFFFFF" width="513" height="114"/>
+      <rect y="0" fill="#D80027" width="513" height="114"/>
+      <rect y="228" fill="#D80027" width="513" height="114"/>
+    </svg>
+  )
+}
---
+++ packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-icon-flag/TH.tsx
@@ -0,0 +1,14 @@
+import { h } from '@stencil/core';
+
+export default () => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 85.333 512 341.333">
+			<rect y="85.334" fill="#FFFFFF" width="512" height="341.326" />
+			<rect y="194.056" fill="#0052B4" width="512" height="123.882" />
+			<g>
+				<rect y="85.334" fill="#D80027" width="512" height="54.522" />
+				<rect y="372.143" fill="#D80027" width="512" height="54.522" />
+			</g>
+		</svg>
+	)
+}
