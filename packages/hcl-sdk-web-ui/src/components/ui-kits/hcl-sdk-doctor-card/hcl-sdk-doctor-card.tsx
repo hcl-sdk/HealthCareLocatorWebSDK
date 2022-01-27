a3cd276b066bdcabbdc23b53d4568f7af6a7fbf3
Quang Tien Le <quang-tien.le@ekino.com>
2022-01-27 15:28:26 +0700

    fix(sdk-653): not show appointment if no link found

--- packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-doctor-card/hcl-sdk-doctor-card.tsx
+++ packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-doctor-card/hcl-sdk-doctor-card.tsx
@@ -34,9 +34,20 @@ export class HclSdkDoctorCard {
             <div class="header">
               <span class="text name">{this.name}&nbsp;&nbsp; { isShowLogo && <img src="https://www.mapatho.com/favicon.ico" alt="" /> }</span>
               {/* TODO: Appointment link feature */}
-              <a href={this.url} target='_blank' onClick={e => e.stopPropagation()}>
-                <hcl-sdk-button round icon="calendar-clock-outline" noBackground noBorder  />
-              </a>
+              {this.url && (
+                <a
+                  href={this.url}
+                  target="_blank"
+                  onClick={e => e.stopPropagation()}
+                >
+                  <hcl-sdk-button
+                    round
+                    icon="calendar-clock-outline"
+                    noBackground
+                    noBorder
+                  />
+                </a>
+              )}
             </div>
 
             <span class="text gp">{this.specialtyPrimary}</span>
