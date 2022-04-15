069949ae4b6dedf7792d0ce5e3bc707676727fde
Quang Tien Le <quang-tien.le@ekino.com>
2022-04-15 17:24:34 +0700

    fix(sdk-945): fix sdk-api endpoint

--- packages/hcl-sdk-core/src/graphql/constants.ts
+++ packages/hcl-sdk-core/src/graphql/constants.ts
@@ -1,1 +1,3 @@
-export const ENDPOINT = (window as any).HCL_API_GRAPHQL_HOST || process.env.API_GRAPHQL_HOST;
+const VERSION = 'v2';
+const graphqlEndpoint = (window as any).HCL_API_GRAPHQL_HOST || process.env.API_GRAPHQL_HOST;
+export const ENDPOINT = graphqlEndpoint + `/${VERSION}/query`;
