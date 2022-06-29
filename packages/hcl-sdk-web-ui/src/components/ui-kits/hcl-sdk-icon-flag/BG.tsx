9c072f75cbe2abe8c2f99c6a6fedaed1fbf85fc8
Quang Tien Le <quang-tien.le@ekino.com>
2022-06-29 14:17:02 +0700

    feat(sdk-1330): add flag 14 countries

---
+++ packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-icon-flag/BG.tsx
@@ -0,0 +1,9 @@
+import { h } from '@stencil/core';
+
+export default () => (
+  <svg width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="85.333 85.333 341.333 341.333">
+    <rect y="85.337" fill="#00966e" width="512" height="341.326" />
+    <rect y="85.337" fill="#FFFFFF" width="512" height="113.775" />
+    <rect y="312.888" fill="#d62612" width="512" height="113.775" />
+  </svg>
+);
