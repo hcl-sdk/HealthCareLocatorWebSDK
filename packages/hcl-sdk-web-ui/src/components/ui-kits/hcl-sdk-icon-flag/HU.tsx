fd6561bac636fdddbea205a544d44b08dc13fd32
Nguyên Nguyễn <khoi-nguyen.nguyen-hoang@ekino.com>
2022-08-05 12:10:07 +0700

    fix(SDK-1330) fix flag svg with different viewbox to avoid square shape

--- packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-icon-flag/HU.tsx
+++ packages/hcl-sdk-web-ui/src/components/ui-kits/hcl-sdk-icon-flag/HU.tsx
@@ -1,9 +1,9 @@
 import { h } from '@stencil/core';
 
 export default () => (
-  <svg width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="85.333 85.333 341.333 341.333">
-    <rect y="85.337" fill="#FFFFFF" width="512" height="341.326" />
-    <rect y="85.337" fill="#D80027" width="512" height="113.775" />
-    <rect y="312.888" fill="#6DA544" width="512" height="113.775" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 6 3">
+    <rect fill="#477050" width="6" height="3" />
+    <rect fill="#FFF" width="6" height="2" />
+    <rect fill="#CE2939" width="6" height="1" />
   </svg>
 );
