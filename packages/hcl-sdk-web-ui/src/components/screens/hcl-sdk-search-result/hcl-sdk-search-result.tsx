979435ffaa90b066a3518caa33fc05f62acba8dd
Nguyên Nguyễn <khoi-nguyen.nguyen-hoang@ekino.com>
2022-08-22 14:35:09 +0700

    fix(SDK-1057) auto search when go back to search list page from detail card

--- packages/hcl-sdk-web-ui/src/components/screens/hcl-sdk-search-result/hcl-sdk-search-result.tsx
+++ packages/hcl-sdk-web-ui/src/components/screens/hcl-sdk-search-result/hcl-sdk-search-result.tsx
@@ -104,6 +104,7 @@ export class HclSdkSearchResult {
       if (searchMapStore.state.navigateFromHcoFullCard) {
         this.goBackToHcoFullCard();
       } else {
+        this.fetchSearchIfNeeded();
         this.goBackToList();
       }
     }
@@ -230,6 +231,20 @@ export class HclSdkSearchResult {
     this.isLoadingRelaunch = false;
     this.newDragLocation = null;
   };
+
+  fetchSearchIfNeeded = () => {
+    if (searchMapStore.searchTarget === SEARCH_TARGET.HCP) {
+      const {activities} = searchMapStore
+      if (activities.data && !activities.data.length) {
+        searchLocationWithParams()
+      }
+    } else {
+      const hcos = searchMapStore.state.hcos
+      if (hcos && !hcos.length) {
+        HCOApis.searchLocationWithParams()
+      }
+    }
+  }
 
   goBackToList = () => {
     searchMapStore.setState({
@@ -520,7 +535,6 @@ export class HclSdkSearchResult {
 
     const { isLoading: loadingActivities, status: activitiesStatus, data: activities } = searchMapStore.activities;
     const isNoDataAvailable = activitiesStatus === 'unauthorized';
-
     const isShowNoResults = !isAllowDisplayMapEmpty && !loadingActivities && activities && !activities.length && !isShowHCPDetail && !isNoDataAvailable;
     const isShowToolbar = {
       mobile: !loadingActivities && isSmall && !selectedActivity && !isNoDataAvailable,
@@ -624,7 +638,6 @@ export class HclSdkSearchResult {
     }
 
     const { selectedActivity, selectedHco, searchFields } = searchMapStore.state;
-
     const selectedAddressName = searchMapStore.isSearchNearMe ? t('near_me') : searchFields.address;
 
     const breakpoint = uiStore.state.breakpoint;
@@ -637,7 +650,6 @@ export class HclSdkSearchResult {
     });
 
     const isShowHeaderBlockMobile = this.shouldShowHeaderBlockMobile();
-
     return (
       <Host class={wrapperClass}>
         {(!(selectedActivity || selectedHco) || !isSmall) &&
